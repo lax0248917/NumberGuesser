b31f4c9d891a2b983cf8d1ca51e1322b52859a53
lax0248917 <lax0248917@gmail.com>
2020-07-30 23:17:54 -0400

    :rocket: Initial C# application commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            // Set App Vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Zach King";
+
+            // Change Text Color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out App Info
+            Console.WriteLine("{0} : Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Reset Console Color
+            Console.ResetColor();
+
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string input = Console.ReadLine();
+
+            // Write out Greeting
+            Console.WriteLine("Hello {0}. Let's play a game...", input);
+        }
+    }
+}
